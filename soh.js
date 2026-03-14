bf0e467abc2a11fa0dfe68be43b4a1eb08f23998
zalo <zalo@users.noreply.github.com>
2026-03-14 15:43:03 +0000

    deploy: 392a899f068b0e7e32ed66bb6725cdfc00e28640

--- soh.js
+++ soh.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmptbl82v56.js
+// include: /tmp/tmpqi4o3ox5.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -421,21 +421,21 @@ Module['FS_createPath']("/assets/xml/N64_PAL_11", "textures", true, true);
 
   })();
 
-// end include: /tmp/tmptbl82v56.js
-// include: /tmp/tmphs9gv6x4.js
+// end include: /tmp/tmpqi4o3ox5.js
+// include: /tmp/tmppjncolnf.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmphs9gv6x4.js
-// include: /tmp/tmp9u6eoo0o.js
+  // end include: /tmp/tmppjncolnf.js
+// include: /tmp/tmp4keb25pf.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp9u6eoo0o.js
+  // end include: /tmp/tmp4keb25pf.js
 
 
 // Sometimes an existing Module object exists with properties
